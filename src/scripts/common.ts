07bcbb5679fb5adaaaaaf06c783f21ae919446b3
M1lken01 <69786783+M1lken01@users.noreply.github.com>
2024-10-31 17:42:13 +0100

    Initial commit

---
+++ src/scripts/common.ts
@@ -0,0 +1,16 @@
+if (localStorage.getItem('darkmode') === null) localStorage.setItem('darkmode', '1');
+let darkmode = localStorage.getItem('darkmode') === '1';
+loadDarkmode();
+(document.querySelector(`a[href="${window.location.pathname.split('/').pop() || 'index.html'}"]`) as HTMLElement).classList.add('underline');
+
+function toggleDarkmode(): void {
+  darkmode = !darkmode;
+  localStorage.setItem('darkmode', darkmode ? '1' : '0');
+  document.querySelector('body')!.dataset.darkmode = String(darkmode);
+  loadDarkmode();
+}
+
+function loadDarkmode(): void {
+  document.querySelector('body')!.dataset.darkmode = String(darkmode);
+  (document.querySelector('footer img') as HTMLImageElement).src = `./imgs/${darkmode ? 'sun' : 'moon'}.png`;
+}
